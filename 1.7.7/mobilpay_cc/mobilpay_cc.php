82ec5cdae45af906d6404c8bfaeef5be23870148
Navid <navid@netopia-system.com>
2023-08-21 13:45:34 +0300

    Fix ver dictation

--- 1.7.7/mobilpay_cc/mobilpay_cc.php
+++ 1.7.7/mobilpay_cc/mobilpay_cc.php
@@ -430,7 +430,7 @@ class Mobilpay_cc extends PaymentModule
                 'samedaysLockerId' => $lockerId,
                 'samedaysLockerName' => $lockerName,
                 'samedaysLockerAddress' => $lockerAddress,
-                'vesion' => "1.0.0",
+                'version' => "1.0.0",
                 'api' => "1.0",
                 'platform'=> "prestaShop " . _PS_VERSION_,
                 'cartSummary'=> $cartSummaryJson
