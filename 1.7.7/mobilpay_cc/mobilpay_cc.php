31b9962352544212785953ac1bf897f689585834
Alexandru I. Neacsu <dark2y@gmail.com>
2024-10-17 11:28:51 +0300

    Update mobilpay_cc.php fixed #11
    Fixed typo referenced in https://github.com/mobilpay/prestashop/issues/11

--- 1.7.7/mobilpay_cc/mobilpay_cc.php
+++ 1.7.7/mobilpay_cc/mobilpay_cc.php
@@ -424,7 +424,7 @@ class Mobilpay_cc extends PaymentModule
 
             // $objPmReqCard->orderId = intval($params['cart']->id) . '#' .$lockerId.'#'.$lockerName.'#' .$lockerAddress.'#'. time();
             $objPmReqCard->orderId = intval($params['cart']->id) . '#'. time();
-            $objPmReqCard->returnUrl = (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://') . htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT,'UTF-8') . __PS_BASE_URI__ . 'index.php?controller=order-confirmation?key=' . $customer->secure_key . '&amp;id_cart=' . intval($params['cart']->id) . '&amp;id_module=' . intval($this->id);
+            $objPmReqCard->returnUrl = (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://') . htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT,'UTF-8') . __PS_BASE_URI__ . 'index.php?controller=order-confirmation&key=' . $customer->secure_key . '&amp;id_cart=' . intval($params['cart']->id) . '&amp;id_module=' . intval($this->id);
         
             $objPmReqCard->params = array(
                 'samedaysLockerId' => $lockerId,
